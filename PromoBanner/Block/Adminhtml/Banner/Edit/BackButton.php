56e84e808bfc7fcc3ee1d467664f8081be55f46d
mamun4343 <al.mamun0514@gmail.com>
2021-08-05 17:52:56 +0600

    4

--- PromoBanner/Block/Adminhtml/Banner/Edit/BackButton.php
+++ PromoBanner/Block/Adminhtml/Banner/Edit/BackButton.php
@@ -6,6 +6,7 @@ use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
 
 class BackButton extends GenericButton implements ButtonProviderInterface
 {
+    
     /**
      * @return array
      */
